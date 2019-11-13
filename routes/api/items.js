0a5b61653f24c0f84823e2f6938082b4fd158859
cliffgor <clierozigy@gmail.com>
2019-11-13 14:06:46 +0300

    modified the delete request method

--- routes/api/items.js
+++ routes/api/items.js
@@ -37,7 +37,7 @@ router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({success: true})))
         .catch (err => res.status(404).json({ success:false}));
-})
+});
 
 
 

fcdfd412d0080d3b0ce4cb98dfc09b48c8ad3d71
cliffgor <clierozigy@gmail.com>
2019-11-26 20:08:25 +0300

    added a deleteItem action in the ShoppingList.js file so that you could delete an item

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,7 +1,16 @@
 import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
 
+// Get item Actions
 export const getItems = () => {
     return{
         type: GET_ITEMS
+    };
+};
+
+// Delete Item Actions
+export const deleteItems = (id) => {
+    return{
+        type: DELETE_ITEM,
+        payload: id
     };
 };
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import uuid from 'uuid';
 import { connect } from 'react-redux';
-import { getItems }  from '../actions/itemActions';
+import { getItems, deleteItem }  from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 
@@ -74,4 +73,5 @@ ShoppingList.propTypes = {
 const mapStateToProps = (state) => ({
     item: state.item
 });
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem }
+    )(ShoppingList);

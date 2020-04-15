3ea041a29bf18c92519680e8dbfa2b0166ca6aae
cliffgor <clierozigy@gmail.com>
2020-04-15 10:18:46 +0300

    updated the item reducers and added axios package

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,25 +1,30 @@
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from './types';
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "./types";
 
 // Get item Actions
 export const getItems = () => {
-    return{
-        type: GET_ITEMS
-    };
+  return {
+    type: GET_ITEMS,
+  };
 };
 
 // Delete Item Actions
-export const deleteItem = id => {
-    return{
-        type: DELETE_ITEM,
-        payload: id
-    };
+export const deleteItem = (id) => {
+  return {
+    type: DELETE_ITEM,
+    payload: id,
+  };
 };
-  
 
 // ADD ITME ACTION
-export const addItem = item => {
-    return{
-        type: ADD_ITEM,
-        payload: item
-    };
+export const addItem = (item) => {
+  return {
+    type: ADD_ITEM,
+    payload: item,
+  };
+};
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING,
+  };
 };
